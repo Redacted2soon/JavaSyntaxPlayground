1e6316eff133099b26a04277d24b79eda0b68024
Ethan <Ethan@Ethans_PC>
2023-08-05 18:08:51 -0400

    second

---
+++ TestProject/src/FirstTest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner stringScanner = new Scanner(System.in);
+
+String name = stringScanner.next();
+
+System.out.println("hello world your name is : "+name);
+	}
+
+}
