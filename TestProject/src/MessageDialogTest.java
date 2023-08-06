5a1c919d8b7daa9437c08453a85ba23fa99c205c
Ethan <Ethan@DESKTOP-4VJSI6S.lan>
2023-08-06 16:43:08 -0400

    Message dialog practice

---
+++ TestProject/src/MessageDialogTest.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class MessageDialogTest {
+
+	public static void main(String[] args) {
+		
+		String name = JOptionPane.showInputDialog("enter your name " );
+		
+		JOptionPane.showMessageDialog(null,"Hello "+name );
+		
+		
+		int age =  Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+		
+		JOptionPane.showMessageDialog(null,"you are "+age+ " years old");
+		
+		Double height =  Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+		
+		JOptionPane.showMessageDialog(null,"you are "+height+ " cm tall");
+		
+	}
+}
