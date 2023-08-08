621396a81273670a6c40e58aed87df88c09b8f9b
Redacted2soon <117399290+Redacted2soon@users.noreply.github.com>
2023-08-07 22:41:38 -0400

    Update FirstTest.java
    test comment

--- TestProject/src/FirstTest.java
+++ TestProject/src/FirstTest.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class FirstTest {
-
+//test comment
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Scanner stringScanner = new Scanner(System.in);
